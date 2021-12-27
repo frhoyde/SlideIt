38289f5acf84ee1e4532a8cec6659c828680a766
ntanjila <nahidaakter@iut-dhaka.edu>
2021-12-27 19:57:00 +0600

    basic registration page initiated #1

--- slide_it/src/App.js
+++ slide_it/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from "react"; 
 import './App.css';
 
 function App() {
+  const[usernameReg, setUsernameReg] = useState('');
+  const[passwordReg, setPasswordReg] = useState('');
+  const register = () => {
+    Axios.post("http://localhost:3001/register", {
+      username: usernameReg,
+      passord: passwordReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="registration">
+        <h1>Registration</h1>
+        <label>Username</label>
+        <input type="text"
+          onChange={(e) => {
+          setUsernameReg(e.target.value);
+        }} 
+        />
+        <label>password</label>
+        <input type="text"
+          onChange={(e) => {
+          setPasswordReg(e.target.value);
+        }} 
+        />
+        <button> Register </button>
+      </div>
+      <div className="login">
+        <h1>Login</h1>
+        <input type="text" placeholder="Username..."/>
+        <input type="password" placeholder="Password..."/>
+        <button> Register </button>
+      </div>
     </div>
   );
 }
