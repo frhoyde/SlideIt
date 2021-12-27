4f35ed5760479dc7edaad426a7a44edee1badd6a
ntanjila <nahidaakter@iut-dhaka.edu>
2021-12-27 23:26:19 +0600

    UI modified #1

--- slide_it/src/App.js
+++ slide_it/src/App.js
@@ -1,41 +1,98 @@
-import React, { useState } from "react"; 
-import './App.css';
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+
+import "./App.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: { 
+    padding: "30" ,
+    "& > *": {
+      width: theme.spacing(45),
+      height: theme.spacing(100),
+    },
+  },
+}));
 
 function App() {
-  const[usernameReg, setUsernameReg] = useState('');
-  const[passwordReg, setPasswordReg] = useState('');
-  const register = () => {
-    Axios.post("http://localhost:3001/register", {
-      username: usernameReg,
-      passord: passwordReg,
-    }).then((response) => {
-      console.log(response);
-    });
-  };
-
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+  // const register = () => {
+  //   Axios.post("http://localhost:3001/register", {
+  //     username: usernameReg,
+  //     passord: passwordReg,
+  //   }).then((response) => {
+  //     console.log(response);
+  //   });
+  // };
+  const classes = useStyles();
   return (
     <div className="App">
-      <div className="registration">
-        <h1>Registration</h1>
-        <label>Username</label>
-        <input type="text"
-          onChange={(e) => {
-          setUsernameReg(e.target.value);
-        }} 
-        />
-        <label>password</label>
-        <input type="text"
-          onChange={(e) => {
-          setPasswordReg(e.target.value);
-        }} 
-        />
-        <button> Register </button>
-      </div>
-      <div className="login">
-        <h1>Login</h1>
-        <input type="text" placeholder="Username..."/>
-        <input type="password" placeholder="Password..."/>
-        <button> Register </button>
+      <div className={classes.root} id="Regcard">
+        <Paper elevation={15}>
+          <div className="form_container">
+            <div className="registration">
+              <h1>Registration</h1>
+              <TextField
+                className="input"
+                id="standard-password-input"
+                label="Username"
+                type="text"
+                autoComplete="current-password"
+                onChange={(e) => {
+                  setUsernameReg(e.target.value);
+                }}
+              />
+              <TextField
+                className="input input_2"
+                id="standard-password-input"
+                label="password"
+                type="password"
+                autoComplete="current-password"
+                onChange={(e) => {
+                  setPasswordReg(e.target.value);
+                }}
+              />
+
+              <br />
+              <Button variant="contained" color="primary">
+                {" "}
+                Register{" "}
+              </Button>
+            </div>
+            <br/>
+            <div className="login">
+              <h1>Login</h1>
+              <TextField
+                className="input"
+                id="standard-password-input"
+                label="Username"
+                type="text"
+                autoComplete="current-password"
+                onChange={(e) => {
+                  setUsernameReg(e.target.value);
+                }}
+              />
+              <TextField
+                className="input input_2"
+                id="standard-password-input"
+                label="password"
+                type="password"
+                autoComplete="current-password"
+                onChange={(e) => {
+                  setPasswordReg(e.target.value);
+                }}
+              />
+              <br/>
+              <Button variant="contained" color="primary">
+                {" "}
+                Log in{" "}
+              </Button>
+            </div>
+          </div>
+        </Paper>
       </div>
     </div>
   );
