882031852c7569475bf1fa3882f621d233540d6e
SazidFarhan <sazidfarhan.a7c@gmail.com>
2022-01-13 21:43:43 +0600

    password hash implemented using bcypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -23,6 +24,18 @@ const UserSchema = new mongoose.Schema({
     resetPasswordExpire: Date
    
 
+});
+
+UserSchema.pre("save", async function(next) {
+    if(!this.isModified("password"))
+    {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+    
 });
 
 
