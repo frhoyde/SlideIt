8e83f5046f56d90c86d22dd91ec0c13cc2671446
SazidFarhan <sazidfarhan.a7c@gmail.com>
2022-01-13 16:07:23 +0600

    Initializing file system template

---
+++ controllers/auth.js
@@ -0,0 +1,16 @@
+exports.register = (req, res, next) => {
+    res.send("Register Route");
+};
+
+exports.login = (req, res, next) => {
+    res.send("Login Route");
+};
+
+exports.forgotpassword = (req, res, next) => {
+    res.send("Forgot Password Route");
+};
+
+exports.resetpassword = (req, res, next) => {
+    res.send("Reset Password Route");
+}; 
+
