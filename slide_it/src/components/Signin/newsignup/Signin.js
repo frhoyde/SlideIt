5fbca0e3b515dc59b900d618d4297d823be917fa
ntanjila <nahidaakter@iut-dhaka.edu>
2022-03-01 00:23:59 +0600

    sign in error fixed #12

--- slide_it/src/components/Signin/newsignup/Signin.js
+++ slide_it/src/components/Signin/newsignup/Signin.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Form from './Forms'
+import Form from './Form'
 
-const Signin = () => {
+const Signin = ({history}) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
