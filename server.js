75a92fea0a131e39915091f2b9cc377b2bc12687
Poushi <afrinsultana@iut-dhaka.edu>
2022-03-01 19:23:11 +0600

    backEnd started Database  #2 New Presentation #30

--- server.js
+++ server.js
@@ -1,35 +1,80 @@
-require('dotenv').config({ path: "./config.env"});
+require("dotenv").config({ path: "./config.env" });
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const mongoose = require('mongoose');
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/error');
-
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+const Socketio = require("socket.io");
+const Document = require("./Document");
+const http = require("http");
 
 // connect to database
 connectDB();
 
-
 const app = express();
 const port = process.env.PORT || 5500;
+
+//const ser = http.createServer(app);
 
 app.use(cors());
 app.use(express.json());
 
-
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/private', require('./routes/private'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
 
 // Error Handler
 app.use(errorHandler);
 const uri = process.env.ATLAS_URI;
 
-
-const server = app.listen(port, () => {console.log(`Server is running on port ${port}`);});
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 process.on("unhandledRejection", (err, promise) => {
-    console.log(`Logged error: ${err}`);
-    server.close(() => process.exit(1));
-})
+  console.log(`Logged error: ${err}`);
+  server.close(() => process.exit(1));
+});
+
+/**** For Document Server *****/
+
+mongoose.connect("mongodb://localhost:3000/document", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const io = require("socket.io")(port, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const defaultValue = "";
+
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+}
