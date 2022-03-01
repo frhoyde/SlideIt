ed2b0895574192d03d705f018a7f0cc932ccb418
ntanjila <nahidaakter@iut-dhaka.edu>
2022-03-02 01:53:11 +0600

    Explore template front end started #23

--- slide_it/src/components/Dashboard/Cards/templateCards.js
+++ slide_it/src/components/Dashboard/Cards/templateCards.js
@@ -1,39 +1,120 @@
-import React from "react";
-
+import React, {useState} from "react";
+import Icon1 from "../../../images/creative1.png";
+import Icon2 from "../../../images/creative2.png";
+import Icon3 from "../../../images/creative3.png";
+import Icon4 from "../../../images/creative4.png";
+import Icon5 from "../../../images/creative5.png";
+import Icon6 from "../../../images/presentation3.jpg";
+import Icon7 from "../../../images/presentation4.jpg";
+import Icon8 from "../../../images/presentation5.jpg";
+import Icon9 from "../../../images/presentation6.jpg";
+import Icon10 from "../../../images/creative6.png";
+import Icon11 from "../../../images/presentation8.jpg";
+import Icon12 from "../../../images/presentation9.jpg";
+import {FaRegHeart
+} from "react-icons/fa";
 import {
-  // templateCard,
-  // templateIcon,
-  // templateWrapper,
-  // ServicesH1,
-  TemplateContainer,
+  ServicesCard,
+  ServicesContainer,
+  ServicesIcon,
+  ServicesWrapper,
+  ServicesH2,
+  ServicesP,
+  ServicesH1,
 } from "./templateCardElement";
 
-import "./templateCard.css";
-
-const TemplateCard = (props) => {
+const Services = () => {
+  const [style, setStyle] = useState({display: 'none'});
   return (
     <>
-      <TemplateContainer id="templates" className="container">
-        {/* <ServicesH1>
-          Here are some recommended templates to start up{" "}
-        </ServicesH1> */}
-        <templateWrapper>
-          <div className="card" style={{ maxWidth: "20rem" }}>
-            <img src={props.img} alt="template" />
-            <div className="card-body">
-              <h2>{props.title}</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam.
-              </p>
-              <h5>{props.author}</h5>
-            </div>
-          </div>
-        </templateWrapper>
-      </TemplateContainer>
+      <div >
+        <ServicesContainer id="services">
+          <ServicesWrapper>
+            <ServicesCard>
+              <ServicesIcon src={Icon7} />
+              <ServicesH2>Basic Dark</ServicesH2>
+               <button  icon={<FaRegHeart/>}></button>
+
+              
+            </ServicesCard>
+            <ServicesCard>
+              <ServicesIcon src={Icon8} />
+              <ServicesH2>Astronomy</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+            <ServicesCard>
+              <ServicesIcon src={Icon9} />
+              <ServicesH2>Classic wedding</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+          </ServicesWrapper>
+        </ServicesContainer>
+        <ServicesContainer id="services">
+          <ServicesWrapper>
+            <ServicesCard>
+              <ServicesIcon src={Icon10} />
+              <ServicesH2>Creative</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+            <ServicesCard>
+              <ServicesIcon src={Icon11} />
+              <ServicesH2>Royal</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+            <ServicesCard>
+              <ServicesIcon src={Icon12} />
+              <ServicesH2>Fun Doodle</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+          </ServicesWrapper>
+        </ServicesContainer>
+
+        <ServicesContainer id="services">
+          <ServicesWrapper>
+            <ServicesCard>
+              <ServicesIcon src={Icon4} />
+              <ServicesH2>Design plan</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+            <ServicesCard>
+              <ServicesIcon src={Icon5} />
+              <ServicesH2>Teach Kids</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+            <ServicesCard>
+              <ServicesIcon src={Icon6} />
+              <ServicesH2>Lights</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+          </ServicesWrapper>
+        </ServicesContainer>
+        <ServicesContainer id="services">
+          <ServicesWrapper>
+            <ServicesCard>
+              <ServicesIcon src={Icon1} />
+              <ServicesH2>Artistics effects</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+            <ServicesCard>
+              <ServicesIcon src={Icon2} />
+              <ServicesH2>Employee Handbook</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+            <ServicesCard>
+              <ServicesIcon src={Icon3} />
+              <ServicesH2>Company Meeting</ServicesH2>
+              <button  icon={<FaRegHeart/>}></button>
+            </ServicesCard>
+          </ServicesWrapper>
+        </ServicesContainer>
+
+        {/* <div class="artist-card">
+  <h2 class="artist-card__title">St. Vincent</h2>
+  <img class="artist-card__img" src="https://drive.google.com/uc?export=view&id=1u5BffcNc9U0DNGBgsrd-r833PSb0b3K1" alt="St. Vincent"></img>
+</div> */}
+      </div>
     </>
   );
 };
 
-export default TemplateCard;
+export default Services;
