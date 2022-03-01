817a9fa07a7e19d48e9d8679b98345083371b1d8
Poushi <afrinsultana@iut-dhaka.edu>
2022-03-02 00:13:03 +0600

    minor style changed #23

--- slide_it/src/components/Dashboard/avatar.js
+++ slide_it/src/components/Dashboard/avatar.js
@@ -12,10 +12,9 @@ export default class avatar extends Component {
         color="#F47100"
         round={true}
         style={{
-          
           float: "right",
           display: "absoulate",
-          marginLeft: "25rem ",
+          marginLeft: "30rem ",
         }}
       />
     );
