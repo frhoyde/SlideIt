21261c967694ac92618b8ff812d9f46c6ddd5478
SazidFarhan <sazidfarhan.a7c@gmail.com>
2022-03-01 14:42:29 +0600

    forgot password linked to backend #1

--- slide_it/src/components/Signin/newsignup/ForgotPass.js
+++ slide_it/src/components/Signin/newsignup/ForgotPass.js
@@ -1,56 +1,105 @@
 import React from 'react';
-import { Formik, Form } from 'formik';
-import { TextField } from './TextField';
-import * as Yup from 'yup';
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Form from './Form';
 
-export const ForgotPass = () => {
-   const validate = Yup.object({  
-    firstName: Yup.string()
-    .min(3, 'Must be 15 characters or less')
-    .max(15, 'Must be 15 characters or less')
-    .required('Required'),
-    lastName: Yup.string()
-      .max(20, 'Must be 20 characters or less')
-      .required('Required'),
-    email: Yup.string()
-      .email('Email is invalid')
-      .required('Email is required'),
-    password: Yup.string()
-      .min(6, 'Password must be at least 6 charaters')
-      .required('Password is required'),
-    confirmPassword: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Password does not match')
-      .required('Confirm password is required'),
-    })
-  
-  return (
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-      }}
-      validationSchema={validate}
-      onSubmit={values => {
-        console.log(values)
-      }}
-    >
-      {formik => (
-        <div>
-          <h1 className="my-4 font-weight-bold .display-4">Reset Password</h1>
-          <Form>
-           
-            <TextField label="Email" name="email" type="email" />
-            
-            <button className="btn btn-success mt-3" type="submit">Send Code</button> <span>     </span>
-            <button className="btn btn-danger mt-3 ml-3" type="reset">Reset</button>
-          </Form>
+const ForgotPass = () => {
+
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+    const [validate, setValidate] = useState({});
+
+    const validateforgotPassword = () => {
+        let isValid = true;
+
+        let validator = Form.validator({
+            email: {
+                value: email,
+                isRequired: true,
+                isEmail: true
+            }
+        });
+
+        if (validator !== null) {
+            setValidate({
+                validate: validator.errors
+            });
+
+            isValid = false;
+        }
+        return isValid;
+    };
+
+    const forgotPasswordHandler = async (e) => {
+        e.preventDefault();
+
+        const config = {
+            header: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        try {
+            const { data } = await axios.post(
+                "/api/auth/forgotpassword",
+                { email },
+                config
+            );
+
+            setSuccess(data.data);
+        } catch (error) {
+            setError(error.response.data.error);
+            setEmail("");
+            setTimeout(() => {
+                setError("");
+            }, 5000);
+        }
+    };
+
+    return (
+        <div className="row g-0 auth-wrapper">
+            <div className="col-12 col-md-5 col-lg-6 h-100 auth-background-col">
+                <div className="auth-background-holder"></div>
+                <div className="auth-background-mask"></div>
+            </div>
+
+            <div className="col-12 col-md-7 col-lg-6 auth-main-col text-center">
+                <div className="d-flex flex-column align-content-end">
+                    <div className="auth-body mx-auto">
+                        <p>Forgot Password</p>
+                        <div className="auth-form-container text-start">
+                            <form className="auth-form" method="POST" onSubmit={forgotPasswordHandler} autoComplete={'off'}>
+                                <div className="email mb-3">
+                                    <input type="email"
+                                        className={`form-control ${validate.validate && validate.validate.email ? 'is-invalid ' : ''}`}
+                                        id="email"
+                                        name="email"
+                                        value={email}
+                                        placeholder="Email"
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+
+                                    <div className={`invalid-feedback text-start ${(validate.validate && validate.validate.email) ? 'd-block' : 'd-none'}`} >
+                                        {(validate.validate && validate.validate.email) ? validate.validate.email[0] : ''}
+                                    </div>
+                                </div>
+                                
+                                <div className="text-center">
+                                    <button type="submit" className="btn btn-primary w-100 theme-btn mx-auto">Forgot Password</button>
+                                </div>
+                            </form>
+
+                            <hr />
+                            <div className="auth-option text-center pt-2"><Link className="text-link" to="/login" >Back to Login</Link></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-      )}
-    </Formik>
-  )
+    );
 }
 
-export default ForgotPass
+export default ForgotPass;
