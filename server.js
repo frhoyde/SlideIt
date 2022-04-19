b0550f381e2b66d003ff4522d74d0da7ddb76bc9
Poushi <afrinsultana@iut-dhaka.edu>
2022-04-20 01:53:01 +0600

    Socket io connected with client side server

--- server.js
+++ server.js
@@ -57,20 +57,25 @@ const io = require("socket.io")(3001, {
 const defaultValue = "";
 
 io.on("connection", (socket) => {
-  console.log("ConnecTed");
-  socket.on("get-document", async (documentId) => {
-    const document = await findOrCreateDocument(documentId);
-    socket.join(documentId);
-    socket.emit("load-document", document.data);
+  console.log("Done Socket ConnecTed");
 
-    socket.on("send-changes", (delta) => {
-      socket.broadcast.to(documentId).emit("receive-changes", delta);
-    });
-
-    socket.on("save-document", async (data) => {
-      await Document.findByIdAndUpdate(documentId, { data });
-    });
+  socket.on("get-paragraph", (slideNumber, item) => {
+    io.emit("receive-paragraph", slideNumber, item);
+    console.log(slideNumber, item);
   });
+  // socket.on("get-document", async (documentId) => {
+  //   const document = await findOrCreateDocument(documentId);
+  //   socket.join(documentId);
+  //   socket.emit("load-document", document.data);
+
+  //   socket.on("send-changes", (delta) => {
+  //     socket.broadcast.to(documentId).emit("receive-changes", delta);
+  //   });
+
+  //   socket.on("save-document", async (data) => {
+  //     await Document.findByIdAndUpdate(documentId, { data });
+  //   });
+  // });
 });
 
 async function findOrCreateDocument(id) {
